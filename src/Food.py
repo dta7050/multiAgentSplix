799d7d4382adcb01df38b49af8024fd9907e01ed
dannabel <dominick.annabel@ibm.com>
2019-11-04 20:49:57 -0500

    commented Food and Snake

--- src/Food.py
+++ src/Food.py
@@ -6,49 +6,71 @@ food point from the foodlist once the point is eaten '''
 import Constants
 
 from numpy.random import randint
+from typing import List
 
 from Point import Point
+from Snake import Snake
 
 
 class Food:
 
-    def __init__(self, snakes=[]):
-        self.foodList = []
+    def __init__(self, snakes: List[Snake] = []):
+        """
+        Initializes the Food object
+        :param snakes: list of Snake objects
+        """
+        self.foodList = []  # type: List[Point]  # Contains all points that are food in the game
         self.createFood(Constants.maximumFood, snakes)
 
-    ''' This method spawns specified number of food points in the
-    grid at random positions. It also ensures that the food points
-    created are not overlapping '''
-    def createFood(self, n, snakes=[]):
-        occupiedPoints = []
+    def createFood(self, n: int, snakes: List[Snake] = []) -> None:
+        """
+        This method spawns specified number of food points in the grid at random positions.
+        It also ensures that the food points created are not overlapping
+        :param n: number of food points to make
+        :param snakes: List of all Snakes in game. Used to create list of occupied points so that food points
+        do not get placed on top of snakes
+        :return: None
+        """
+        # We dont want to place food where Snake bodies already are. This gets a list of all occupied points
+        occupied_points = []  # type: List[Point]
         for snake in snakes:
             body = snake.getBodyList()
             bodyPoints = Point.returnBodyPoints(body)
-            occupiedPoints.extend(bodyPoints)
+            occupied_points.extend(bodyPoints)
 
+        # Generate random food points
         for i in range(n):
             while True:
                 x = randint(1, Constants.gridSize-1)
                 y = randint(1, Constants.gridSize-1)
                 p = Point(x,y)
-                if p not in occupiedPoints and p not in self.foodList:
+                if p not in occupied_points and p not in self.foodList:
                     self.foodList.append(p)
                     break
 
-    ''' This method is used to update the foodlist either when new
-    food points get added  or when the dead snake's body points are
-    converted to food points '''
-    def addFoodToList(self, pointList):
+    def addFoodToList(self, pointList: List[Point]) -> None:
+        """
+        This method is used to update the food list either when new food points get added
+        or when the dead snake's body points are converted to food points
+        :param pointList: list of food points
+        :return: None
+        """
         for p in pointList:
             self.foodList.append(p)
 
-    ''' This method deletes the food from the foodlist once it has
-    been eaten. It also maintains a minimum number of food points in
-    the grid at an instance '''
-    def eatFood(self, food, snakes=[]):
+    def eatFood(self, food_point: Point, snakes: List[Snake] = []) -> None:
+        """
+        This method deletes the food from the food list once it has been eaten.
+        It also maintains a minimum number of food points in the grid at an instance
+        :param food_point: Point that is a food point that will be eaten
+        :param snakes: List of all Snakes in game
+        :return: None
+        """
+        # Remove the eaten food point from food list
         for i, f in enumerate(self.foodList):
-            if f == food:
+            if f == food_point:
                 del self.foodList[i]
 
-        if(len(self.foodList) < Constants.maximumFood):
+        # Add a new food point to the game to replace it
+        if len(self.foodList) < Constants.maximumFood:
             self.createFood(Constants.maximumFood - len(self.foodList), snakes)
--- src/Snake.py
+++ src/Snake.py
@@ -12,38 +12,57 @@ from numpy.random import randint
 
 from Point import Point
 from Action import Action
+from Food import Food
+from typing import List, Tuple
+
 
 class Snake:
-    snakeList = []
+    snakeList = []  # contains all snakes created for the game
 
-    def __init__(self, identity):
-        occupiedPoints = []
+    def __init__(self, identity: int):
+        """
+        Initializes the Snake object
+        :param identity: integer number that identifies a Snake object
+        """
+        # Get all points of current snakes in game, store them in occupiedPoints list
+        occupiedPoints = []  # type: List[Point]
         for snake in Snake.snakeList:
             body = snake.getBodyList()
-            bodyPoints = Point.returnBodyPoints(body)
+            bodyPoints = Point.returnBodyPoints(body)  # type: List[Point]
             occupiedPoints.extend(bodyPoints)
 
+        # create the body of this snake instance
         while True:
-            self.head = Point(randint(10, Constants.gridSize - 10), randint(10, Constants.gridSize - 10)) # generate point with at least 10 units gap from any wall
-            self.end = Point( self.head.x - 5, self.head.y )
+            # generate point with at least 10 units gap from any wall
+            self.head = Point(randint(10, Constants.gridSize - 10), randint(10, Constants.gridSize - 10))
+            self.end = Point(self.head.x - 5, self.head.y)
             self.joints = []
+            # First, the body is made up of a head point, end point, and joints (none yet)
             body = self.getBodyList()
+            # Next, the returnBodyPoints function calculates all intermediate points of the snake
             bodyPoints = Point.returnBodyPoints(body)
+            # If the generated body list does not contain any "occupiedPoints" break this loop;
+            # otherwise generate a new body. (This could be improved)
             if not bool(self.lists_overlap(bodyPoints, occupiedPoints)):
                 break
 
-        self.id = identity
-        self.alive = True
-        self.score = 0
+        self.id = identity  # type: int  # assigns id number to snake
+        self.alive = True   # tells whether the snake is alive or not (True = alive)
+        self.score = 0      # contains the score of the snake
+        # the following attributes contain the previous positional state of the snake
         self.prev_head, self.prev_joints, self.prev_end = self._copy(self.head, self.joints, self.end)
-
+        # add Snake object to list containing all created snakes
         Snake.snakeList.append(self)
         return
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Returns status message of Snake containing life status, position and score
+        :return: str, the status message
+        """
         if self.alive:
             body = self.getBodyList()
-            body_str = str(map(str,body))
+            body_str = str(map(str, body))
             print_message = "Snake {}\n\tPosition = {}\n\tScore = {}\n".format(self.id, body_str, self.score)
             return print_message
         else:
@@ -51,35 +70,64 @@ class Snake:
             return print_message
 
     ''' Helper method to check if the bodies of the snake overlap '''
-    def lists_overlap(self, list1, list2):
+    def lists_overlap(self, list1: List[Point], list2: List[Point]) -> bool:
+        """
+        Returns True if list1 and list2 contain any equivalent Points, returns False otherwise
+        :param list1: first list to compare
+        :param list2: second list to compare
+        :return: bool
+        """
         for point in list1:
             if point in list2:
                 return True
         return False
 
     ''' Returns the body of the snake stitching together the head, tail and the joints '''
-    def getBodyList(self):
+    def getBodyList(self) -> List[Point]:
+        """
+        Returns list only containing the head, end, and joint points of a Snake.
+        NOTE: Does not return connecting points!
+        :return: List[Point]
+        """
         body = [self.head]
         body.extend(self.joints)
         body.append(self.end)
         return body
 
     ''' Returns a boolean indicating if a food point has been eaten by a snake '''
-    def didEatFood(self, food):
+    # TODO: WE WILL NOT NEED THIS FUNCTION FOR OUR GAME !!
+    def didEatFood(self, food) -> bool:
         return (self.head in food.foodList)
 
     ''' Increments the score of a snake '''
-    def incrementScore(self, reward):
+    def incrementScore(self, reward: int) -> None:
+        """
+        Adds a reward to the Snake's score
+        :param reward: integer value corresponding to the reward to add to the score
+        :return: None
+        """
         self.score += reward
         return
 
     ''' Returns a boolean indicating if the snake hit a wall '''
-    def didHitWall(self):
-        return (self.head.x == 0 or self.head.x == Constants.gridSize or self.head.y == 0 or self.head.y == Constants.gridSize)
+    def didHitWall(self) -> bool:
+        """
+        Returns True if the Snakes head hit a wall
+        Returns False otherwise
+        :return: bool
+        """
+        return (self.head.x == 0 or self.head.x == Constants.gridSize or
+                self.head.y == 0 or self.head.y == Constants.gridSize)
 
     ''' Method to update the point coordinates according to the direction
     of movement '''
-    def _update_point(self, p, direction):
+    def _update_point(self, p: Point, direction: int) -> Point:
+        """
+        Updates a point's coordinates depending on the Action the Snake is taking
+        :param p: Point object to move
+        :param direction: integer corresponding to the direction to move p in
+        :return: the moved point, Point
+        """
         if direction == Action.TOP:
             p.y += 1
         elif direction == Action.DOWN:
@@ -90,68 +138,105 @@ class Snake:
             p.x -= 1
         return p
 
-    def _copy(self, head, joints, end):
+    def _copy(self, head, joints, end) -> Tuple[Point, List[Point], Point]:
+        """
+        Returns a copy of the Snakes head, joints and endpoint
+        :param head: Point corresponding to head of Snake
+        :param joints: Points corresponding to joints of Snake
+        :param end: Point corresponding to endpoint of Snake
+        :return: Tuple containing (Head, Joints, End)
+        """
         _head = Point.fromPoint(head)
         _joints = [Point.fromPoint(p) for p in joints]
         _end = Point.fromPoint(end)
         return _head, _joints, _end
 
-    def backtrack(self):
+    def backtrack(self) -> None:
+        """
+        Sets the current head, joints, an end points of a Snake to its previous head, joints and end points
+        :return: None
+        """
         self.head, self.joints, self.end = self._copy(self.prev_head, self.prev_joints, self.prev_end)
         return
 
     ''' Moves the snake in a direction chosen by it at each time step. This updates
     the snake body points '''
-    def moveInDirection(self, action):
+    def moveInDirection(self, action: Action) -> None:
+        """
+        Adjusts the Snakes head, joints and end point given an Action to move in a direction
+        :param action: Action for Snake to take
+        :return: None
+        """
+        # First, check to see if we can make the given action
         assert (action in self.permissible_actions()), "Action not allowed in this state."
+        # Store current state of snake as the new previous state of snake before taking action
         self.prev_head, self.prev_joints, self.prev_end = self._copy(self.head, self.joints, self.end)
 
         # move the snake in the direction specified
-        if self.joints == []:
+        if self.joints == []:  # Then Snake is a straight line horizontal or vertical
+            # get current direction the Snake is heading
             direction = self.findDirection(self.head, self.end)
-            if direction != action: # add joint when snake changes direction
+            if direction != action:  # add joint when snake changes direction
                 self.joints.append(Point.fromPoint(self.head))
+            # Update head and end points
             self.head = self._update_point(self.head, action)
             self.end = self._update_point(self.end, direction)
-        else:
+        else:  # Then Snake can be any shape with any number of joints
+            # get current direction the Snake is heading
             direction = self.findDirection(self.head, self.joints[0])
             if direction != action: # add joint when snake changes direction
                 self.joints.insert(0, Point.fromPoint(self.head))
             self.head = self._update_point(self.head, action)
-
             direction = self.findDirection(self.joints[-1], self.end)
             self.end = self._update_point(self.end, direction)
-            if (self.end.x == self.joints[-1].x) and (self.end.y == self.joints[-1].y): # pop joint if end has reached it
+            if (self.end.x == self.joints[-1].x) and (self.end.y == self.joints[-1].y):  # pop joint if end reached it
                 self.joints = self.joints[:-1]
 
         return
 
     ''' Returns a boolean indicating if the snake hit another snake '''
-    def didHitSnake(self, opponent_snake):
-        body = opponent_snake.getBodyList()
+    def didHitSnake(self, opponent_snake: 'Snake') -> bool:
+        """
+        If this Snake has collided with 'opponent_snake' return True.
+        Else, return False.
+        :param opponent_snake: Other Snake to check collision with
+        :return: bool
+        """
+        # get head, joints, and end of opponent snake
+        body = opponent_snake.getBodyList()  # type: List[Point]
 
-        p = self.head
-        if p == opponent_snake.head :
-            return (self.score <= opponent_snake.score) # if heads collide, the larger snake remains. If the scores are equal, then both snakes should die.
+        p = self.head  # type: Point
+        # If Snake's heads collide...
+        if p == opponent_snake.head:
+            # TODO: CHANGE THIS DEPENDING ON OUR RULE
+            # if heads collide, the larger snake remains. If the scores are equal, then both snakes should die.
+            return self.score <= opponent_snake.score
 
         for i in range(len(body)-1):
-            p1 = body[i]
-            p2 = body[i+1]
-            if p1.x == p2.x: #vertical line
-                if p.x == p1.x:
+            p1 = body[i]  # point of opponent snake
+            p2 = body[i+1]  # next point of opponent snake
+            if p1.x == p2.x:  # points make up a vertical line of opponent snake
+                if p.x == p1.x:  # then head of snake is at the same x coord of opponent
+                    # check if snake head has collided with opponent snake points
                     lim1, lim2 = tuple(sorted([p1.y, p2.y]))
-                    if p.y in range( lim1, lim2 +1):
+                    if p.y in range(lim1, lim2 + 1):
                         return True
-            else: #horizontal line
-                if p.y == p1.y:
+            else:  # points make up a horizontal line of opponent snake
+                if p.y == p1.y:  # then head of snake is at the same y coord of opponent
+                    # check if snake head has collided with opponent snake points
                     lim1, lim2 = tuple(sorted([p1.x, p2.x]))
                     if p.x in range(lim1, lim2 + 1):
                         return True
-
         return False
 
     ''' Returns the direction of P1 with reference to P2 '''
-    def findDirection(self, p1, p2):
+    def findDirection(self, p1: Point, p2: Point) -> int:
+        """
+        Returns the direction of P1 with reference to P2
+        :param p1: Point to be compared to
+        :param p2: Point to compare with p1
+        :return: integer value corresponding to direction
+        """
         if p1.x - p2.x == 0 and p1.y - p2.y < 0:
             return Action.DOWN
         elif p1.x - p2.x == 0 and p1.y - p2.y > 0:
@@ -163,6 +248,7 @@ class Snake:
 
     ''' Grows the snake in the direction of the tail once the food is eaten.
     Should be called before moveInDirection '''
+    # TODO: WE WILL NOT NEED THIS IF WE CHANGE THE GAME
     def growSnake(self):
         if self.joints == []:
             direction = self.findDirection(self.end, self.head)
@@ -171,28 +257,38 @@ class Snake:
             direction = self.findDirection(self.end, self.joints[-1])
         self.end = self._update_point(self.end, direction)
 
-    ''' Returns the permissible actions for a snake, given the direction of motion
-    of the snake '''
-    def permissible_actions(self):
-        actions = []
-        if self.joints == []:
+    def permissible_actions(self) -> List[Action]:
+        """
+        Returns the permissible actions for a snake, given the direction of motion of the snake
+        :return: list of actions the snake can take, List[Action]
+        """
+        actions = []  # type: List[Action]
+        if self.joints == []:  # Then snake is just a vertical or horizontal line
             direction = self.findDirection(self.end, self.head)
         else:
             direction = self.findDirection(self.joints[0], self.head)
+        # Permissible actions are those that move the Snake not back the same way it is coming from
         for act in (Action):
             if act != direction:
                 actions.append(act)
         return actions
 
-    ''' Once the snake is dead, this method sets the alive bit of the snake to false,
-    converts the body points into food points and deletes the snake's head, joints and tail '''
-    def killSnake(self, food):
-        self.alive = False
+    # TODO: WILL NEED TO CHANGE IF WE DO SPLIX GAME
+    def killSnake(self, food: Food) -> None:
+        """
+        Once the snake is dead, this method sets the alive bit of the snake to false,
+        converts the body points into food points and deletes the snake's head, joints and tail
+        :param food:
+        :return: None
+        """
+        self.alive = False  # kill Snake
 
+        # Convert body points into Food
         body = self.getBodyList()
         points = Point.returnBodyPoints(body)
         food.addFoodToList(points)
 
+        # Delete Snake
         Snake.snakeList.remove(self)
         del self.head
         del self.end
