16285e529035d0d5d6ec341adf22e222b787319c
Bobby <b.novak23964@gmail.com>
2019-11-06 13:29:03 -0500

    Did more work on ActorCritic.py

--- src/ActorCritic.py
+++ src/ActorCritic.py
@@ -102,41 +102,68 @@ def getValueFunction(state: ndarray, w: ndarray) -> float:
     return np.dot(w.T, featureVector)
 
 
-def getAction(snake, state, theta):
+def getAction(snake: Snake, state: ndarray, theta: ndarray) -> Action:
     """
-
-    :param snake:
-    :param state:
-    :param theta:
-    :return:
+    Function that creates a list of a snakes possible actions, computes the probability of each action,
+    and then determines the best action to take given the state of the game
+    :param snake: The current snake
+    :param state: The current state of the environment
+    :param theta: Array of parameters to quantify how good each state-action pair is
+    :return: The action to be taken
     """
     actionProbability = []
     actions = []
-    for action in snake.permissible_actions():
-        actionProbability.append(getPolicy(snake, state, theta, action))
-        actions.append(action)
-    return Action(np.random.choice(actions, p=actionProbability))
+    for action in snake.permissible_actions():  # iterate through each action
+        actionProbability.append(getPolicy(snake, state, theta, action))  # compute the probability of the action
+        actions.append(action)  # add the action to the list
+    return Action(np.random.choice(actions, p=actionProbability))  # determine which action to take
 
 
 ''' Returns the differential of the policy with respect to the policy parameter theta'''
 
 
-def getGradientForPolicy(snake, state, action, theta):
+def getGradientForPolicy(snake: Snake, state: ndarray, action: Action, theta: ndarray) -> ndarray:
+    """
+    Function that, given an action and a state, computes the gradient which is used to update
+    the agent's policy. This is assumed to be analogous to using gradient descent to minimize
+    an error function
+    :param snake: The current snake
+    :param state: The current state of the environment
+    :param action: The action for which the calculation will take place
+    :param theta: Array of parameters to quantify how good each state-action pair is
+    :return: The computed gradient used to update the agent's policy
+    """
     featureVector = getFeatureVector(state, action)
     exps = np.exp(getNumericalPreferences(snake, state, theta))
-    feature_exps = np.asarray([ getFeatureVector(state, action) * exps[i] for i, action in enumerate(snake.permissible_actions()) ] )
+    feature_exps = np.asarray([getFeatureVector(state, action) * exps[i]
+                              for i, action in enumerate(snake.permissible_actions())])
     numr = np.sum(feature_exps, axis=0)
     denr = np.sum(exps)
     return featureVector - (numr / denr)
 
+
 ''' Actor critic algorithm is implemented in this method and the agent is set to
 train according to the algorithm. It also saves the checkpoints while training '''
-def train(maxTimeSteps, checkpointFrequency=500, checkpoint_dir="checkpoints", load=False, load_dir="checkpoints", load_time_step=500):
-    length = getStateLength()
-    theta = np.zeros((Constants.numberOfSnakes, length * 2))
-    w = np.zeros((Constants.numberOfSnakes, length))
 
-    if load: # resume training from old checkpoints
+
+def train(maxTimeSteps: int, checkpointFrequency: int = 500, checkpoint_dir="checkpoints",
+          load=False, load_dir="checkpoints", load_time_step=500):
+    """
+    Trains the snakes for a given amount of time. Initializes the training parameters, w and theta,
+    to zero, and updates them after every action taken by the snakes
+    :param maxTimeSteps: The amount of time the snakes are trained
+    :param checkpointFrequency: How often the training parameters are saved
+    :param checkpoint_dir: Path of where to save the training data
+    :param load: Whether to load pre-trained snakes or train new ones from scratch
+    :param load_dir: Path where the training data to load would be
+    :param load_time_step: The starting time step upon load
+    :return: null
+    """
+    length = getStateLength()  # returns the length of the state (amount of features the state has)
+    theta = np.zeros((Constants.numberOfSnakes, length * 2))  # creates matrix of zeros(num snakes x 2(length of state))
+    w = np.zeros((Constants.numberOfSnakes, length))  # creates matrix of zeros (num snakes x length of states)
+
+    if load:  # resume training from old checkpoints
         w = np.load("{}/w_{}.npy".format(load_dir, load_time_step))
         theta = np.load("{}/theta_{}.npy".format(load_dir, load_time_step))
 
@@ -145,38 +172,38 @@ def train(maxTimeSteps, checkpointFrequency=500, checkpoint_dir="checkpoints", l
         try:
             shutil.rmtree(checkpoint_dir)
         except OSError as e:
-            print ("Error: %s - %s." % (e.filename, e.strerror))
+            print("Error: %s - %s." % (e.filename, e.strerror))
     os.makedirs(checkpoint_dir)
 
-    timeSteps = 0
+    timeSteps = 0  # initialize the time counter
     counter = 0
     while timeSteps <= maxTimeSteps:
-        g = Game()
+        g = Game()  # initialize the game
         episodeRunning = True
 
         while episodeRunning:
-            I = 1
-            actionList = []
-            stateList = []
+            I = 1  # used in calculating theta
+            actionList = []  # empty list to store actions of each snake
+            stateList = []  # empty list to hold each snake's perception of the environment
             for i, snake in enumerate(g.snakes):
                 if not snake.alive:
                     actionList.append(None)
                     stateList.append([-1] * getStateLength())
                     continue
-                opponentSnakes = [opponent for opponent in g.snakes if opponent != snake]
-                stateList.append(getState(snake, opponentSnakes, g.food, normalize=True))
-                action = getAction(snake, stateList[i], theta[i])
-                actionList.append(action)
+                opponentSnakes = [opponent for opponent in g.snakes if opponent != snake]  # finds the opponent snakes
+                stateList.append(getState(snake, opponentSnakes, g.food, normalize=True))  # gets the current state
+                action = getAction(snake, stateList[i], theta[i])  # compute the snake's action
+                actionList.append(action)  # adds the action to the list
 
-            singleStepRewards, episodeRunning = g.move(actionList)
-            timeSteps += 1
+            singleStepRewards, episodeRunning = g.move(actionList)  # snakes perform their actions
+            timeSteps += 1  # increment the counter
             print("t = " + str(timeSteps))
 
-            if timeSteps % checkpointFrequency == 0:
+            if timeSteps % checkpointFrequency == 0:  # save the training parameters
                 np.save("{}/theta_{}.npy".format(checkpoint_dir, timeSteps), theta)
                 np.save("{}/w_{}.npy".format(checkpoint_dir, timeSteps), w)
 
-            for i, snake in enumerate(g.snakes):
+            for i, snake in enumerate(g.snakes):  # get the rewards and use them to update theta and w
                 if not snake.alive:
                     continue
                 opponentSnakes = [opponent for opponent in g.snakes if opponent != snake]
@@ -189,7 +216,7 @@ def train(maxTimeSteps, checkpointFrequency=500, checkpoint_dir="checkpoints", l
                 theta[i] += Constants.AC_alphaTheta * I * delta * getGradientForPolicy(snake, state, action, theta[i])
                 I *= Constants.gamma
 
-            if timeSteps > maxTimeSteps:
+            if timeSteps > maxTimeSteps:  # break the loop and end the game
                 break
 
         g.endGame()
